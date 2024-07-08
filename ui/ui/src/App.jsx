fce6c58c246cf8964356ec311ce342bd61a6c9dc
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-07-07 22:14:41 -0500

    ui

---
+++ ui/ui/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import EventDetails from './components/EventDetails';
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <EventDetails/>
+      </div>
+      
+    </>
+  )
+}
+export default App
